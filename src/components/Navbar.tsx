5dfceaa3edd6bd3e4b39d2a2bef729c433410b76
Ashish Jha <ashishxyzjha@gmail.com>
2025-09-01 17:47:37 +0530

    Navbar Animation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,13 +2,14 @@
 
 import Image from "next/image"
 import { useState } from "react";
+import { motion } from "framer-motion";
 
 
 function Navbar({ active, setActive }: { active: string; setActive: (val: string) => void }) {
   const [open, setOpen] = useState(false)
 
   return (
-    <div className="fixed top-3 left-4 right-4 py-3 mx-auto px-4 h-15 bg-black rounded-xl z-50">
+    <div className="fixed top-4 left-4 right-4 py-3 mx-auto px-4 h-15 bg-black rounded-xl z-50">
         <div className="flex justify-between">
         <div className="flex justify-center items-center cursor-pointer gap-3">
           <Image src='/logo.jpg' alt="Profile" width={24} height={24} className="rounded-full" />
@@ -16,26 +17,26 @@ function Navbar({ active, setActive }: { active: string; setActive: (val: string
         </div>
 
         <div className="hidden md:flex justify-end gap-3">
-        <a href="#about" onClick={() => setActive("about")} className={`flex items-center gap-3 cursor-pointer rounded-sm px-4 py-2 ${active === "about" ? "bg-[#F5E901] text-black" : "hover:bg-[#DAED6D] hover:text-black"}`}>
+        <motion.a href="#about" onClick={() => setActive("about")} className={`flex items-center gap-3 cursor-pointer rounded-sm px-4 py-2 transform transition-all duration-500 ${active === "about" ? "bg-[#F5E901] text-black" : "hover:bg-[#DAED6D] hover:text-black"}`}>
           <div className="rounded-full h-3 w-3 bg-neutral-700"></div>
           <div className="font-semibold">About</div>
-        </a>
-        <a href="#skills" onClick={() => setActive("skills")} className={`flex items-center gap-3 cursor-pointer rounded-sm px-4 py-2 ${active === "skills" ? "bg-[#F5E901] text-black" : "hover:bg-[#DAED6D] hover:text-black"}`}>
+        </motion.a>
+        <motion.a href="#skills" onClick={() => setActive("skills")} className={`flex items-center gap-3 cursor-pointer rounded-sm px-4 py-2 transform transition-all duration-500 ${active === "skills" ? "bg-[#F5E901] text-black" : "hover:bg-[#DAED6D] hover:text-black"}`}>
           <div className="rounded-full h-3 w-3 bg-neutral-700"></div>
           <div className="font-semibold">Skills</div>
-        </a>
-        <a href="#projects" onClick={() => setActive("projects")} className={`flex items-center gap-3 cursor-pointer rounded-sm px-4 py-2 ${active === "projects" ? "bg-[#F5E901] text-black" : "hover:bg-[#DAED6D] hover:text-black"}`}>
+        </motion.a>
+        <motion.a href="#projects" onClick={() => setActive("projects")} className={`flex items-center gap-3 cursor-pointer rounded-sm px-4 py-2 transform transition-all duration-500 ${active === "projects" ? "bg-[#F5E901] text-black" : "hover:bg-[#DAED6D] hover:text-black"}`}>
           <div className="rounded-full h-3 w-3 bg-neutral-700"></div>
           <div className="font-semibold">Projects</div>
-        </a>
-        <a href="#experience" onClick={() => setActive("experience")} className={`flex items-center gap-3 cursor-pointer rounded-sm px-4 py-2 ${active === "experience" ? "bg-[#F5E901] text-black" : "hover:bg-[#DAED6D] hover:text-black"}`}>
+        </motion.a>
+        <motion.a href="#experience" onClick={() => setActive("experience")} className={`flex items-center gap-3 cursor-pointer rounded-sm px-4 py-2 transform transition-all duration-500 ${active === "experience" ? "bg-[#F5E901] text-black" : "hover:bg-[#DAED6D] hover:text-black"}`}>
           <div className="rounded-full h-3 w-3 bg-neutral-700"></div>
           <div className="font-semibold">Experience</div>
-        </a>
-        <a href="#contact" onClick={() => setActive("contact")} className={`flex items-center gap-3 cursor-pointer rounded-sm px-4 py-2 ${active === "contact" ? "bg-[#F5E901] text-black" : "hover:bg-[#DAED6D] hover:text-black"}`}>
+        </motion.a>
+        <motion.a href="#contact" onClick={() => setActive("contact")} className={`flex items-center gap-3 cursor-pointer rounded-sm px-4 py-2 transform transition-all duration-500 ${active === "contact" ? "bg-[#F5E901] text-black" : "hover:bg-[#DAED6D] hover:text-black"}`}>
           <div className="rounded-full h-3 w-3 bg-neutral-700"></div>
           <div className="font-semibold">Contact</div>
-        </a>
+        </motion.a>
 
         </div>
           <div onClick={() => setOpen(!open)} className="flex md:hidden items-center cursor-pointer pr-4">
@@ -45,26 +46,26 @@ function Navbar({ active, setActive }: { active: string; setActive: (val: string
 
         {open && (
         <div className="flex flex-col mt-3 py-3 gap-2 md:hidden bg-black">
-        <a href="#about" onClick={() => setActive("about")} className={`flex items-center gap-3 cursor-pointer rounded-sm w-80 self-center px-4 py-2 border border-[#DAED6D] ${active === "about" ? "bg-[#F5E901] text-black" : "hover:bg-[#DAED6D] hover:text-black"}`}>
+        <motion.a href="#about" onClick={() => setActive("about")} className={`flex items-center gap-3 cursor-pointer rounded-sm w-80 self-center px-4 py-2 transform transition-all duration-500 border border-[#DAED6D] ${active === "about" ? "bg-[#F5E901] text-black" : "hover:bg-[#DAED6D] hover:text-black"}`}>
           <div className="rounded-full h-3 w-3 bg-neutral-700"></div>
           <div className="font-semibold">About</div>
-        </a>
-        <a href="#skills" onClick={() => setActive("skills")} className={`flex items-center gap-3 cursor-pointer rounded-sm w-80 self-center px-4 py-2 border border-[#DAED6D] ${active === "skills" ? "bg-[#F5E901] text-black" : "hover:bg-[#DAED6D] hover:text-black"}`}>
+        </motion.a>
+        <motion.a href="#skills" onClick={() => setActive("skills")} className={`flex items-center gap-3 cursor-pointer rounded-sm w-80 self-center px-4 py-2 transform transition-all duration-500 border border-[#DAED6D] ${active === "skills" ? "bg-[#F5E901] text-black" : "hover:bg-[#DAED6D] hover:text-black"}`}>
           <div className="rounded-full h-3 w-3 bg-neutral-700"></div>
           <div className="font-semibold">Skills</div>
-        </a>
-        <a href="#projects" onClick={() => setActive("projects")} className={`flex items-center gap-3 cursor-pointer rounded-sm w-80 self-center px-4 py-2 border border-[#DAED6D] ${active === "projects" ? "bg-[#F5E901] text-black" : "hover:bg-[#DAED6D] hover:text-black"}`}>
+        </motion.a>
+        <motion.a href="#projects" onClick={() => setActive("projects")} className={`flex items-center gap-3 cursor-pointer rounded-sm w-80 self-center px-4 py-2 transform transition-all duration-500 border border-[#DAED6D] ${active === "projects" ? "bg-[#F5E901] text-black" : "hover:bg-[#DAED6D] hover:text-black"}`}>
           <div className="rounded-full h-3 w-3 bg-neutral-700"></div>
           <div className="font-semibold">Projects</div>
-        </a>
-        <a href="#experience" onClick={() => setActive("experience")} className={`flex items-center gap-3 cursor-pointer rounded-sm w-80 self-center px-4 py-2 border border-[#DAED6D] ${active === "experience" ? "bg-[#F5E901] text-black" : "hover:bg-[#DAED6D] hover:text-black"}`}>
+        </motion.a>
+        <motion.a href="#experience" onClick={() => setActive("experience")} className={`flex items-center gap-3 cursor-pointer rounded-sm w-80 self-center px-4 py-2 transform transition-all duration-500 border border-[#DAED6D] ${active === "experience" ? "bg-[#F5E901] text-black" : "hover:bg-[#DAED6D] hover:text-black"}`}>
           <div className="rounded-full h-3 w-3 bg-neutral-700"></div>
           <div className="font-semibold">Experience</div>
-        </a>
-        <a href="#contact" onClick={() => setActive("contact")} className={`flex items-center gap-3 cursor-pointer rounded-sm px-4 w-80 self-center py-2 border border-[#DAED6D] ${active === "contact" ? "bg-[#F5E901] text-black" : "hover:bg-[#DAED6D] hover:text-black"}`}>
+        </motion.a>
+        <motion.a href="#contact" onClick={() => setActive("contact")} className={`flex items-center gap-3 cursor-pointer rounded-sm px-4 w-80 self-center py-2 transform transition-all duration-500 border border-[#DAED6D] ${active === "contact" ? "bg-[#F5E901] text-black" : "hover:bg-[#DAED6D] hover:text-black"}`}>
           <div className="rounded-full h-3 w-3 bg-neutral-700"></div>
           <div className="font-semibold">Contact</div>
-        </a>
+        </motion.a>
         </div>
       )}
     </div>
